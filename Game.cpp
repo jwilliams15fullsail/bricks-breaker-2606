eaa8c3fbf3a9f04b09f3bf800d5e92050d1ea36c
Jeffrey Williams <JWilliams15@student.fullsail.edu>
2026-08-07 19:45:42 -0400

    Todo #3 Completed Updated the Render to render all the bricks replacing the single brick rendering to render loop over the vector

--- Game.cpp
+++ Game.cpp
@@ -82,7 +82,8 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
-	brick.Draw();
+	for (const Box& brick : bricks)
+		brick.Draw();
 
 	Console::Lock(false);
 }
