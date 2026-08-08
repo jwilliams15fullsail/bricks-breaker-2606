9ffb99ea37ab46e980c23bf1ae975c46a9892314
Jeffrey Williams <JWilliams15@student.fullsail.edu>
2026-08-07 21:18:49 -0400

    Fixed Some bugs with the text of Won and Lost

--- Game.cpp
+++ Game.cpp
@@ -104,35 +104,35 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	for(int i = 0; i < bricks.size(); i++){
-	if (bricks[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
-	{
-		bricks[i].color = ConsoleColor(bricks[i].color - 1);
-		ball.y_velocity *= -1;
+	for (int i = 0; i < bricks.size(); i++) {
+		if (bricks[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			bricks[i].color = ConsoleColor(bricks[i].color - 1);
+			ball.y_velocity *= -1;
 
-		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-		if (bricks[i].color == ConsoleColor::Black)
-			bricks.erase(bricks.begin() + i);
-		break;
+			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+			if (bricks[i].color == ConsoleColor::Black)
+				bricks.erase(bricks.begin() + i);
+			break;
+		}
+		}
 
-	}
+		// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
+		if (bricks.empty()) {
+			ball.moving = false;
+			won = true;
+		}
 
-	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
-	if (bricks.empty()) {
-		ball.moving = false;
-		won = true;
-	}
+		if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
+		{
+			ball.y_velocity *= -1;
+		}
 
-	if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
-	{
-		ball.y_velocity *= -1;
-	}
-
-	// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
-	if (ball.y_position >= WINDOW_HEIGHT - 1)
-	{
-		ball.moving = false;
-		lost = true;
-	}
+		// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
+		if (ball.y_position >= WINDOW_HEIGHT - 1)
+		{
+			ball.moving = false;
+			lost = true;
+		}
 	
 }
